708e971523d284348c6f581333a02f6c47a55ce2
Akash Kumar <akakumar.42berlin@gmail.com>
2025-09-29 23:33:27 +0200

    added 553

---
+++ dynamic-programming/optimal-division-leetcode-553.py
@@ -0,0 +1,7 @@
+def optimalDivision(nums) -> str:
+        if len(nums) == 1:
+            return str(nums[0])
+        elif len(nums) == 2:
+            return str(nums[0]) + "/" + str(nums[1])
+        else:
+            return str(nums[0]) + "/(" + "/".join([str(num) for num in nums[1:]]) + ")"
