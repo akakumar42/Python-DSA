c06e18781edea0a8758596e0832ae4cff34b8b05
Akash Kumar <akakumar.42berlin@gmail.com>
2025-10-01 12:32:25 +0200

    Cal. the function after a certain elem rotation in an array

---
+++ dynamic-programming/rotate-func-leetcode-396.py
@@ -0,0 +1,16 @@
+def maxRotateFunction(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        total = sum(nums)
+        
+        f = sum(i * num for i, num in enumerate(nums))
+        res = f
+        
+        for k in range(1, n):
+            f = f + total - n * nums[n - k]   
+            res = max(res, f)
+        
+        return res
